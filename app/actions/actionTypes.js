55802ccac376bb1e2a40ff1deaec258855dc3df2
khasjanovdamir <bestdamir23@gmail.com>
2019-02-02 17:50:33 +0300

    fix actionTypes

--- app/actions/actionTypes.js
+++ app/actions/actionTypes.js
@@ -1,6 +1,7 @@
-export const SET_USER: string = '@USER/SET_USER';
-export const SET_BIRTH: string = '@USER/SET_USER';
-export const SET_SEX: string = '@USER/SET_USER';
-export const SET_CITIZENSHIP: string = '@USER/SET_USER';
-export const SET_DOCUMENT_TYPE: string = '@USER/SET_USER';
-export const SET_DOCUMENT_EXPIRY: string = '@USER/SET_USER';
+export const SET_USER: string = 'SET_USER';
+export const SET_BIRTH: string = 'SET_BIRTH';
+export const SET_SEX: string = 'SET_SEX';
+export const SET_CITIZENSHIP: string = 'SET_CITIZENSHIP';
+export const SET_DOCUMENT_TYPE: string = 'SET_DOCUMENT_TYPE';
+export const SET_DOCUMENT_NUMBER: string = 'SET_DOCUMENT_NUMBER';
+export const SET_DOCUMENT_EXPIRY: string = 'SET_DOCUMENT_EXPIRY';
